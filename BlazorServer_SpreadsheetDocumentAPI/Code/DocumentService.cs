ca8dcbf1789d6c7447a6f25c85e280e4c5e94317
Roman Tabunnikov <roman.tabunnikov@devexpress.com>
2020-11-24 16:38:00 +0300

    Add project files.

---
+++ BlazorServer_SpreadsheetDocumentAPI/Code/DocumentService.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+using DevExpress.Spreadsheet;
+
+namespace BlazorServer_SpreadsheetDocumentAPI.Code {
+    public class DocumentService {
+        public async Task<byte[]> GetXlsxDocumentAsync(double loanAmount, int periodInYears, double interestRate, DateTime startDateOfLoan) {
+            using var workbook = await GenerateDocumentAsync(loanAmount, periodInYears, interestRate, startDateOfLoan);
+            return await workbook.SaveDocumentAsync(DocumentFormat.Xlsx);
+        }
+
+        public async Task<byte[]> GetPdfDocumentAsync(double loanAmount, int periodInYears, double interestRate, DateTime startDateOfLoan) {
+            using var workbook = await GenerateDocumentAsync(loanAmount, periodInYears, interestRate, startDateOfLoan);
+            using var ms = new MemoryStream();
+            await workbook.ExportToPdfAsync(ms);
+            return ms.ToArray();
+        }
+
+        public async Task<byte[]> GetHtmlDocumentAsync(double loanAmount, int periodInYears, double interestRate, DateTime startDateOfLoan) {
+            using var workbook = await GenerateDocumentAsync(loanAmount, periodInYears, interestRate, startDateOfLoan);
+            using var ms = new MemoryStream();
+            await workbook.ExportToHtmlAsync(ms, workbook.Worksheets[0]);
+            return ms.ToArray();
+        }
+
+        async Task<Workbook> GenerateDocumentAsync(double loanAmount, int periodInYears, double interestRate, DateTime startDateOfLoan) {
+            var workbook = new Workbook();
+            await workbook.LoadDocumentAsync("Data/LoanAmortizationSchedule_template.xltx");
+            new LoanAmortizationScheduleDocumentGenerator(workbook)
+                .GenerateDocument(loanAmount, periodInYears, interestRate, startDateOfLoan);
+            return workbook;
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,7 @@
+# BlazorServer and Spreadsheet Document API
+
+[Index.razor](/BlazorServer_SpreadsheetDocumentAPI/Pages/Index.razor) - component + code behind
+
+[DocumentService.cs](/BlazorServer_SpreadsheetDocumentAPI/Code/DocumentService.cs) - document generation service
+
+[ExportController.cs](/BlazorServer_SpreadsheetDocumentAPI/Api/ExportController.cs) - WebAPI controller
